0a6c885b4007e4ebfe98b4756806aafe63408bee
always18yo <102338636+always18yo@users.noreply.github.com>
2026-04-05 16:23:09 +0700

    Add files via upload

---
+++ tb/chk/uart_chk_pkg.svh
@@ -0,0 +1,15 @@
+//***************************************************************************************************************
+// Author: Van Le
+// vanleatwork@yahoo.com
+// Phone: VN: 0396221156, US: 5125841843
+//***************************************************************************************************************
+package uart_chk_pkg;
+
+   import uvm_pkg::*;
+   import uart_tlm_pkg::*;
+   import uart_cfg_pkg::*;
+   
+   `include "uvm_macros.svh"
+   `include "sb.sv"
+   
+endpackage
