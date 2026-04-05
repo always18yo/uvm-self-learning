d0b13618d3fb290043790cb032757c03554f1916
always18yo <102338636+always18yo@users.noreply.github.com>
2026-04-05 16:08:41 +0700

    Create uart_cfg.sv

---
+++ tb/cfg/uart_cfg.sv
@@ -0,0 +1,21 @@
+//***************************************************************************************************************
+// Author: Van Le
+// vanleatwork@yahoo.com
+// Phone: VN: 0396221156, US: 5125841843
+//***************************************************************************************************************
+class uart_cfg extends uvm_object;
+
+  `uvm_object_utils(uart_cfg)
+
+  // X Todo: declare a bit variable inject_err
+  
+  //
+  // NEW
+  //
+  function new(string name = "");
+    super.new(name);
+    // X Todo: assign 0 to inject_err
+  endfunction
+
+  
+endclass
